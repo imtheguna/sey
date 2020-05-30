db5cdf0c32635237000d8b2499c1e26deb38e26c
twin-eng <58139175+twin-eng@users.noreply.github.com>
2020-05-30 13:27:45 +0530

    Update cam.sh

--- cam.sh
+++ cam.sh
@@ -317,7 +317,7 @@ fi
 printf "\n"
 printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Serveo.net\e[0m\n"
 printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
-default_option_server="1"
+default_option_server="2"
 read -p $'\n\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Choose a Port Forwarding option: \e[0m' option_server
 option_server="${option_server:-${default_option_server}}"
 
